94d84073af9eb977d8d384b8c33ad69bfac735a2
Matt Tomas <mtomas@companieshouse.gov.uk>
2026-09-23 10:03:33 +0100

    v2 added

---
+++ app/routes/routes-v2-match-link.js
@@ -0,0 +1,279 @@
+//
+// For guidance on how to create routes see:
+// https://prototype-kit.service.gov.uk/docs/create-routes
+//
+
+const govukPrototypeKit = require('govuk-prototype-kit')
+const router = govukPrototypeKit.requests.setupRouter()
+
+module.exports = router
+
+// Show session data and URLs in the terminal  
+router.use((req, res, next) => {  
+    const log = {  
+      method: req.method,  
+      url: req.originalUrl,  
+      data: req.session.data  
+    }  
+    console.log(JSON.stringify(log, null, 2))  
+    next()  
+  }) 
+
+
+/*
+ * Start page
+ */
+router.post('/v2_match_link/scenario', function (req, res) {
+
+    res.redirect('start-page')
+    
+})
+
+
+/*
+ * Start page
+ */
+router.post('/v2_match_link/start-page', function (req, res) {
+
+    res.redirect('email-address')
+    
+})
+
+
+/*
+ * Email address
+ */
+router.post('/v2_match_link/email-address', function (req, res) {
+
+    res.redirect('email-address-confirm')
+    
+})
+
+
+/*
+ * Email address confirm
+ */
+
+router.post('/v2_match_link/email-address-confirm', function (req, res) {
+
+
+    if (req.session.data['scenarios'] === 'dup_email') {
+        res.redirect('email-stop-screen')
+    } else {
+        res.redirect('name')
+    }  
+
+})
+
+
+
+/*
+ * Public name, on the register
+ */
+router.post('/v2_match_link/name', function (req, res) {
+
+    res.redirect('name-on-register')
+    
+})
+
+/*
+ * Public name, on the register
+ */
+router.post('/v2_match_link/name-on-register', function (req, res) {
+
+
+     //Name preference 
+
+     if (req.session.data['registerName'] === 'doc') {
+
+        res.redirect('personal-code')
+
+      } else {
+        // go to the confirm address page
+        res.redirect('name-for-the-register')
+    
+      }
+ 
+})
+
+
+
+/*
+ * Name
+ */
+router.post('/v2_match_link/name-for-the-register', function (req, res) {
+
+    res.redirect('personal-code')
+    
+})
+
+/*
+ * Personal code
+ */
+router.post('/v2_match_link/personal-code', function (req, res) {
+
+    res.redirect('dob')
+    
+})
+
+
+
+
+/*
+ * Date of birth
+ */
+router.post('/v2_match_link/dob', function (req, res) {
+
+    res.redirect('address-lookup')
+    
+})
+
+
+
+/*
+ * lookup  address
+ */
+router.post('/v2_match_link/address-lookup', function (req, res) {
+
+          //if house number is empty
+          if (req.session.data['house-number'] === '') {
+
+            res.redirect('address-list')
+    
+          } else {
+            // go to the confirm address page
+            res.redirect('address-confirm')
+        
+          }
+        
+})
+
+
+/*
+ * Confirm home address
+ */
+router.post('/v2_match_link/address-confirm', function (req, res) {
+
+    if (req.session.data['scenarios'] === 'dup_identity' ||
+        req.session.data['scenarios'] === 'dup_identity_docs') {
+        res.redirect('duplicate-warning')
+    } else {
+      res.redirect('date-identity-checks')
+    }  
+    
+})
+
+
+/*
+ * Duplicate warning
+ */
+router.post('/v2_match_link/duplicate-warning', function (req, res) {
+
+    res.redirect('date-identity-checks')
+    
+})
+
+
+
+/*
+ * Date the documents were checked
+ */
+router.post('/v2_match_link/date-identity-checks', function (req, res) {
+
+    res.redirect('format-of-the-documents-checked')
+    
+})
+
+
+
+
+/*
+ * Format the documents were checked
+ */
+router.post('/v2_match_link/format-of-the-documents-checked', function (req, res) {
+
+     //if house number is empty
+     if (req.session.data['security-check'] === 'Yes') {
+
+        res.redirect('documents-checked-r1')
+
+      } else {
+        // go to the confirm address page
+        res.redirect('documents-checked-r2')
+    
+      }
+
+})
+
+
+
+
+/*
+ * Document group 1
+ */
+router.post('/v2_match_link/documents-checked-r1', function (req, res) {
+
+    res.redirect('id-document-details')
+    
+})
+
+
+
+/*
+ * Document group 2
+ */
+router.post('/v2_match_link/documents-checked-r2', function (req, res) {
+
+    res.redirect('id-document-details')
+    
+})
+
+
+/*
+ * Document details used for IDV
+ */
+router.post('/v2_match_link/id-document-details', function (req, res) {
+
+    if (req.session.data['scenarios'] === 'dup_identity_docs') {
+        res.redirect('duplicate-stop')
+    } else {
+        res.redirect('id-check')
+    }  
+})
+
+
+
+/*
+ * Declaration 
+ */
+router.post('/v2_match_link/id-check', function (req, res) {
+
+    res.redirect('check-your-answers')
+    
+})
+
+
+
+
+/*
+ * Check your answers before sending your application 
+ */
+router.post('/v2_match_link/check-your-answers', function (req, res) {
+
+    res.redirect('confirmation')
+    
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+  module.exports = router
